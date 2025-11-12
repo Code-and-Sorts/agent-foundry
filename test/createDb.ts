345e7a2369349ebadfdf443d2e2fe0f7109b9573
Colby Timm <timmcolby@gmail.com>
2025-11-11 18:04:03 -0800

    fix(tests): update schema file path to use path.join for consistency

--- test/createDb.ts
+++ test/createDb.ts
@@ -1,10 +1,11 @@
 import Database from 'better-sqlite3';
 import fs from 'fs';
+import path from 'path';
 
 export function createTestDb() {
   const db = new Database('file:agent-foundry?mode=memory&cache=shared');
   db.pragma('foreign_keys = ON');
-  const schema = fs.readFileSync('../setup-script.sql', 'utf8');
+  const schema = fs.readFileSync(path.join(__dirname, '../setup-script.sql'), 'utf8');
   db.exec(schema);
   return db;
 }
