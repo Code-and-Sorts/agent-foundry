902db069b69e2e723a3283474b77cf9344878ef9
Colby Timm <timmcolby@gmail.com>
2025-11-11 18:02:59 -0800

    refactor(tests): remove outdated comments in createTestDb function

--- test/createDb.ts
+++ test/createDb.ts
@@ -2,8 +2,6 @@ import Database from 'better-sqlite3';
 import fs from 'fs';
 
 export function createTestDb() {
-  // shared in-memory so multiple connections in the SAME process can see the DB
-  // Note: for true multi-process, use a temp file path instead.
   const db = new Database('file:agent-foundry?mode=memory&cache=shared');
   db.pragma('foreign_keys = ON');
   const schema = fs.readFileSync('../setup-script.sql', 'utf8');
